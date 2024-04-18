6dc3d6559c6f5424cefc29dcb0aeacd8ad6c72f3
Fernando Silva <arch0580@arc-login04.arcus-htc.arc.local>
2024-04-18 14:48:42 +0100

    update create_dic.sh with example for arc-arc

--- get_dic.sh
+++ get_dic.sh
@@ -4,7 +4,8 @@ set -e
 
 usage() {
   echo "Usage: $0 -g <GOALS_FILE> -c <CORRESPONDENCIES_FILE> -f <FINAL_FILE> -p <PATH_RAW>"
-  echo "Example: $0 -g list.txt -c /media/jbod2/eugenio/correspondencies.csv -f dic.txt -p /media/jbod3/joel/ngs/raw_data/sequencing"
+  echo "Example for prime: $0 -g list.txt -c /media/jbod2/eugenio/correspondencies.csv -f dic.txt -p /media/jbod3/joel/ngs/raw_data/sequencing"
+  echo "Example for arc: $0 -g list.txt -c /data/arch-ferrets/arch0580/correspondencies.csv -f dic.txt -p /data/arch-ferrets/backups/sequencing/"
 }
 
 while getopts ":g:c:f:p:" opt; do
