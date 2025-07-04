f7867a827e69bea8911c55edb53c8c57526e0cc2
Eugenio Silva <eugenio@localhost.localdomain>
2025-07-04 14:06:07 +0100

    Cleaned root folder, moved old pipeline to 'old/'

--- scripts_snake/run_FixAdapterRemoval_Merged.py
+++
@@ -1,9 +0,0 @@
-import os
-from snakemake.shell import shell
-from snakemake_wrapper_utils.samtools import get_samtools_opts
-from pathlib import Path
-
-extra = snakemake.params.get("extra", "")
-log = snakemake.log_fmt_shell(stdout=True, stderr=True)
-
-shell("AdapterRemovalFixPrefix {snakemake.input} | sed '/^[@M_+]/ s/M_//g' > {snakemake.output}")
--- scripts_snake/teste.py
+++
@@ -1,3 +0,0 @@
-import sys
-
-print(snakemake.input[0])
